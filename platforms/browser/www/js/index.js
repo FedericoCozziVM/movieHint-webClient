466f3110313b79cd6ce6c9ebe0ff2df9251bf578
FedericoCozziVM <federicocozzivm@gmail.com>
2018-09-03 13:59:38 +0200

    webstorm developing

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -33,7 +33,20 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+        $(document).ready(function(){
+
+            var sessionId = Math.floor(Math.random()*90000) + 10000;
+            sessionStorage.setItem("sessionNumber", sessionId);
+
+            //submit del messaggio
+            $("#userMsgForm").submit(function(event){
+                var input = $("#userMsgInput").val();
+                callToDialogFlow(input);
+                event.preventDefault();
+            });
+        });
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,3 +60,61 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function callToDialogFlow(query){
+    var q = {
+              "lang": "it",
+              "query": query,
+              "sessionId": sessionStorage.getItem("sessionNumber")
+            };
+    var p = JSON.stringify(q);
+    console.log(p);
+    $.ajax(
+        {
+            url: "https://api.dialogflow.com/v1/query?v=20150910",
+            type: 'POST',
+            contentType: "application/json",
+            dataType: "json",
+            headers:{"Authorization" : "Bearer 7772a6c6213840fc8a69eccdbdb950d4"},
+            data: p,
+            success: function(result){
+                console.log(result);
+                var messages;
+                if(result.result.fulfillment.messages){
+                    //svuoto il div che contiene i nuovi messaggi
+                    $("#botSpeech").empty();
+                    messages = result.result.fulfillment.messages;
+
+                    var i;
+                    for(i=0; i<messages.length; i++){
+                        //per ogni messaggio ne identifico il tipo, per capire cosa mostrare
+                        var type = messages[i].type;
+                        switch(type){
+                            case 0:
+                                //messaggio testuale semplice
+                                $("#botSpeech").append("<p>"+messages[i].speech+"</p>");
+                                break;
+                            case 1:
+                                //messaggio tipo card
+                                var card = $("<div></div>").addClass("card").css("width", "18rem");
+                                card.append($("<img>").attr({src : messages[i].imageUrl}));
+                                var cardBody = $("<div></div>").addClass("card-body");
+                                cardBody.append($("<h5></h5>").addClass("card-title").html(messages[i].title));
+                                cardBody.append($("<p></p>").addClass("card-text").html(messages[i].subtitle));
+                                card.append(cardBody);
+                                $("#botSpeech").append(card);
+                            default: break;
+                        }
+                    }
+                }
+
+                $("#userMsgInput").val("");
+            },
+            error: function(xhr){
+                console.log(xhr);
+            }
+        }
+    );
+
+}
+
